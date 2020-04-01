20c7e26dede4995464697b82593e5d614d73aa95
Peyton <peytonbbair@gmail.com>
2020-04-01 15:31:45 -0600

    moved stuff around

--- TradingModel.py
+++ TradingModel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import json
+import numpy as np
 from shutil import copyfile
 import plotly.graph_objs as go
 from plotly.offline import plot
@@ -16,23 +17,26 @@ class TradingModel:
         self.ticker = ticker
         self.df = self.getData()
 
+
     def getData(self):
         #get url to data from kraken api
         ticker = self.ticker
         base = 'https://api.kraken.com'
         endpoint = '/0/public/OHLC'
-        params = '?pair=' + ticker + '&interval=5'
+        params = '?pair=' + ticker + '&interval=15'
         url = base + endpoint + params
         #download the data
         json_string = requests.get(url)
-        a_json = json.loads(json_string.text)
+        dictionary = json.loads(json_string.text)
+        dict_len = len(dictionary['result'][ticker])
+        self.dflength = dict_len
         #creat pandas df
         col_names = ['time', 'open', 'high', 'low', 'close', 'volume', 'sma20', 'sma40', 'rsi', 'obv']
         df = pd.DataFrame(columns = col_names)
 
         #creat df cause the import stuff would work
-        for x in range(720):
-            temp = a_json['result'][ticker][x]
+        for x in range(dict_len):
+            temp = dictionary['result'][ticker][x]
             df = df.append({col_names[0]: temp[0], col_names[1]: temp[1], col_names[2]: temp[2], col_names[3]: temp[3], col_names[4]: temp[4], col_names[5]: temp[5]}, ignore_index=True)
 
         #turn df into floats
@@ -48,17 +52,28 @@ class TradingModel:
     def strategy(self):
         df = self.df
         ticker = self.ticker
+        dflength = self.dflength
 
         buy_signals = []
         sell_signals = []
+        pre_act = 'sell'
+
+        obv_average = df['obv'].sum()/dflength
+        std_obv = np.std(df['obv'])
+        print obv_average
+        print std_obv
+
+
         #check for buy points
         for i in range(1, len(df['close'])):
-			if df['sma40'][i] > df['low'][i] and (df['sma40'][i] - df['low'][i]) > 0.03 * df['low'][i] and df['obv'][i] < 0:
-				buy_signals.append([df['time'][i], df['low'][i]])
-        #check for sell points
-			if df['obv'][i] >= 100000 and df['rsi'][i] >= 65:
-				sell_signals.append([df['time'][i], df['high'][i]])
+            if df['sma40'][i] > df['low'][i] and (df['sma40'][i] - df['low'][i]) > 0.02 * df['low'][i] and df['obv'][i] < -100000:
+                buy_signals.append([df['time'][i], df['low'][i]])
+                pre_act = 'buy'
 
+        #check for sell points
+            if df['obv'][i] >= 100000 and df['rsi'][i] >= 65 and pre_act == 'buy':
+                sell_signals.append([df['time'][i], df['high'][i]])
+                pre_act = 'sell'
 
         self.plotData(buy_signals = buy_signals, sell_signals = sell_signals)
 
@@ -98,19 +113,19 @@ class TradingModel:
             line = dict(color = ('rgba(204, 51, 255, 50)')))
 
         if buy_signals:
-			buys = go.Scatter(
-					x = [item[0] for item in buy_signals],
-					y = [item[1] for item in buy_signals],
-					name = "Buy Signals",
-					mode = "markers",
-				)
+            buys = go.Scatter(
+        		x = [item[0] for item in buy_signals],
+        		y = [item[1] for item in buy_signals],
+        		name = "Buy Signals",
+        		mode = "markers",
+			)
         if sell_signals:
             sells = go.Scatter(
-					x = [item[0] for item in sell_signals],
-					y = [item[1] for item in sell_signals],
-					name = "Sell Signals",
-					mode = "markers",
-                    )
+				x = [item[0] for item in sell_signals],
+				y = [item[1] for item in sell_signals],
+				name = "Sell Signals",
+				mode = "markers",
+                )
 
         #creat plot
         fig = make_subplots( rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.02)
@@ -118,8 +133,11 @@ class TradingModel:
         fig.append_trace(candle, row=1, col=1)
         fig.append_trace(sma20, row=1, col=1)
         fig.append_trace(sma40, row=1, col=1)
-        fig.append_trace(buys, row=1, col=1)
-        fig.append_trace(sells, row=1, col=1)
+        #limit errors when a buy/sell signals doesn't exist
+        if buy_signals:
+            fig.append_trace(buys, row=1, col=1)
+        if sell_signals:
+            fig.append_trace(sells, row=1, col=1)
 
         fig.append_trace(rsi, row=2, col=1)
 
