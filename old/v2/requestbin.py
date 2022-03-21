f35ab46f45fdc4303e5698b1a61dfd7189cb5296
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-21 18:29:32 +0800

    Delete old directory

--- old/v2/requestbin.py
+++
@@ -1,5 +0,0 @@
-import httplib
-
-data = '{ "name": "this is a password" }'
-conn = httplib.HTTPSConnection('enjyrenadmyxd.x.pipedream.net')
-conn.request("POST", "/",data, {'Content-Type': 'application/json'})
